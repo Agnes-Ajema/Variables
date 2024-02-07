49aad35112f6fdf99eeb393afc32ec4cfdc05703
agnes <agnesajemaalukwe@gmail.com>
2024-02-07 23:40:05 +0300

    kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+val fullName = "Agnes Ajema"
+  println(fullName)
+  var age = 27
+  println(age)
+  var phoneNumber = +254716626362
+  println(phoneNumber)
+  var weightKg = 55.2
+  println(weightKg)
+  val kenyanCitizen = true
+  println(kenyanCitizen)
+
+
+
+}
